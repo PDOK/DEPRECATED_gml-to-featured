092db59561a8e78b683354bf3b637a99387b8b9f
Raymond Kroon <raymond.kroon@kadaster.nl>
2015-11-16 17:15:51 +0100

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject featured-gml "0.1.2-SNAPSHOT"
+(defproject featured-gml "0.1.2"
   :description "gml to featured json conversion lib"
   :url "http://github.so.kadaster.nl/PDOK/featured-gml"
   :license {:name "Eclipse Public License"
