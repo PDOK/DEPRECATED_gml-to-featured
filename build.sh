3caf882430b8a504ae152de30ab0145eb8133478
Raymond Kroon <raymond.kroon@kadaster.nl>
2016-02-09 16:38:55 +0100

    chore: fix build ;)

--- build.sh
+++ build.sh
@@ -1,13 +1,1 @@
 #!/usr/bin/env sh
-
-if [ -n "${RELEASE_LEVEL+1}" ]; then
-
-    : "${NEXUS_USERNAME:?Need to set NEXUS_USERNAME non-empty}"
-    : "${NEXUS_PASSWORD:?Need to set NEXUS_PASSWORD non-empty}"
-
-    echo "RELEASING";
-    lein build && lein release ${RELEASE_LEVEL}
-else
-    echo "NORMAL BUILD"
-    lein build
-fi
