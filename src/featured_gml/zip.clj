8d465dabd0be59656d14ce3e6e9cb024671c495e
Niek Hoogma <niek.hoogma@kadaster.nl>
2016-02-26 10:28:51 +0100

    PDOK-2519: Add method for zipping each file in a directory + unit test

--- src/featured_gml/zip.clj
+++ src/featured_gml/zip.clj
@@ -1,5 +1,6 @@
 (ns featured-gml.zip
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.tools.logging :as log]))
 
 (defn entries [zipfile]
  (lazy-seq
@@ -7,11 +8,26 @@
    (cons entry (entries zipfile)))))
 
 (defn zip-directory [zip-file dir]
+  "Put all files in dir into specified zip-file"
   (with-open [zip (java.util.zip.ZipOutputStream. (io/output-stream zip-file))]
     (doseq [f (file-seq (io/file dir)) :when (.isFile f)]
       (.putNextEntry zip (java.util.zip.ZipEntry. (.getName f)))
       (io/copy f zip)
       (.closeEntry zip))))
+
+(defn zip-file [uncompressed-file]
+  "Return zip-file location with zipped content of uncompressed-file"
+  (let [compressed-file (io/file (.getParent uncompressed-file) (str (.getName uncompressed-file) ".zip"))]
+    (log/debug "Compressing file" zip-file)
+    (with-open [zip (java.util.zip.ZipOutputStream. (io/output-stream compressed-file))]
+      (.putNextEntry zip (java.util.zip.ZipEntry. (.getName uncompressed-file)))
+      (io/copy uncompressed-file zip)
+      (.closeEntry zip)
+      compressed-file)))
+
+(defn zip-files-in-directory [dir]
+  "Zip each file in a directory separately"
+  (map #(zip-file %) (filter #(.isFile %) (file-seq (io/file dir)))))
 
 (defn extract-target-file-name [inputname]
   "Extract the target-file name for inputname"
