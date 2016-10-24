c782b59ecc7d7ff736671b3afb648b9c630553aa
Wouter <wouter.visscher@kadaster.nl>
2016-10-24 16:59:12 +0200

    feat: improved config

--- src/gml_to_featured/runner.clj
+++ src/gml_to_featured/runner.clj
@@ -10,6 +10,8 @@
   (:import (javax.xml.stream.events XMLEvent)))
 
 (def ^:dynamic *sequence-selector* identity)
+(def ^:dynamic *feature-selector* identity)
+(def ^:dynamic *feature-identifier* identity)
 (def ^:dynamic *translators* {})
 
 (def unknown nil)
@@ -17,119 +19,137 @@
 (defn unknown-translator [_]
   unknown)
 
-(defn member->map [feature]
-  (let [translator (get *translators* (:tag feature) unknown-translator)
-        translated (translator feature)]
+(defn member->map [fm]
+  (let [feature (*feature-identifier* fm)
+        translator (get *translators* (:tag feature) unknown-translator)
+        translated (translator (*feature-selector* fm))]
     (if (sequential? translated)
       translated
       [translated])))
 
-(defn progress-logger []
-  (let [counter (volatile! 0)]
-    (fn [obj]
-      (vswap! counter inc)
-      (when (= 0 (mod @counter 10000))
-        (log/info "Processed " @counter))
-      obj)))
+  (defn progress-logger []
+    (let [counter (volatile! 0)]
+      (fn [obj]
+        (vswap! counter inc)
+        (when (= 0 (mod @counter 10000))
+          (log/info "Processed " @counter))
+        obj)))
 
-(defn process-stream [stream]
-  (let [log-progress (progress-logger)
-        sequence-selector (if (nil? *sequence-selector*) identity *sequence-selector*)]
-    (->> stream
-         (.createXMLEventReader xml/input-factory)
-         (xml/pull-seq)
-         sequence-selector
-         xml/event->tree
-         :content
-         (mapcat member->map)
-         (map log-progress)
-         (filter #(not= unknown %)))))
+  (defn process-stream [stream]
+    (let [log-progress (progress-logger)
+          sequence-selector (if (nil? *sequence-selector*) identity *sequence-selector*)]
+      (->> stream
+           (.createXMLEventReader xml/input-factory)
+           (xml/pull-seq)
+           sequence-selector
+           xml/event->tree
+           :content
+           (mapcat member->map)
+           (map log-progress)
+           (filter #(not= unknown %)))))
 
-(defn process [reader writer dataset-name validity]
-  (let [first? (ref true)]
-    (.write writer (str "{\"dataset\":\"" dataset-name "\",\n\"features\":["))
-    (doseq [f (process-stream reader)]
-      (if-not @first?
-        (.write writer ",\n")
-        (dosync (ref-set first? false)))
-      (.write writer (json/generate-string (assoc f :_validity validity))))
-    (.write writer "]}")))
+  (defn process [reader writer dataset-name validity]
+    (let [first? (ref true)]
+      (.write writer (str "{\"dataset\":\"" dataset-name "\",\n\"features\":["))
+      (doseq [f (process-stream reader)]
+        (if-not @first?
+          (.write writer ",\n")
+          (dosync (ref-set first? false)))
+        (.write writer (json/generate-string (assoc f :_validity validity))))
+      (.write writer "]}")))
 
-(defn parse-translator-tag [expr]
-  (eval (code/translator (:type expr) (:mapping expr))))
+  (defn parse-translator-tag [expr]
+    (eval (code/translator (:type expr) (:mapping expr))))
 
-(defn parse-translator-nested-tag [mapping]
-  (eval (code/translator mapping)))
+  (defn parse-translator-nested-tag [mapping]
+    (eval (code/translator mapping)))
 
-(defn parse-multi-tag [mappings]
-  (eval (code/multi mappings)))
+  (defn parse-multi-tag [mappings]
+    (eval (code/multi mappings)))
 
-(defn parse-config [config]
-  (edn/read-string
-    {:readers {'xml2json/mappedcollection parse-translator-tag ;this reader-tag is deprecated
-               'xml2json/mapped           parse-translator-tag
-               'xml2json/nested           parse-translator-nested-tag
-               'xml2json/multi parse-multi-tag}} config))
+ (def fns {'first clojure.core/first})
 
-(defn start-element-pred [element-name]
-  (fn [^XMLEvent e]
-    (and (.isStartElement e)
-         (= element-name (keyword (.getLocalPart (.getName (.asStartElement e))))))))
+ (defn upgrade-comp [function-vector]
+   (map (fn [symbol] (get fns symbol symbol)) function-vector))
 
-(defn translate [dataset edn-config validity reader writer]
-  (let [config (parse-config edn-config)
-        translators (if-let [t (:config/translators config)] t config)
-        sequence-element (:config/sequence-element config)
-        sequence-selector (if sequence-element
-                            (partial drop-while (comp not (start-element-pred sequence-element)))
-                            identity)]
-    (binding [*translators* translators
-              *sequence-selector* sequence-selector]
-      (process reader writer dataset validity))))
+  (defn parse-config [config]
+    (edn/read-string
+      {:readers {'xml2json/mappedcollection parse-translator-tag ;this reader-tag is deprecated
+                 'xml2json/mapped           parse-translator-tag
+                 'xml2json/nested           parse-translator-nested-tag
+                 'xml2json/multi            parse-multi-tag
+                 'xml2json/comp             upgrade-comp}} config))
 
-(defn translate-filesystem [dataset edn-config-location validity in-file out-file]
-  (with-open [reader (clojure.java.io/reader in-file),
-              writer (clojure.java.io/writer out-file)]
-    (translate dataset (slurp edn-config-location) validity reader writer))
-  (shutdown-agents))
+  (defn start-element-pred [element-name]
+    (fn [^XMLEvent e]
+      (and (.isStartElement e)
+           (= element-name (keyword (.getLocalPart (.getName (.asStartElement e))))))))
 
-(defn error-msg [errors]
-  (str "The following errors occurred while parsing your command:\n\n"
-       (string/join \newline errors)))
+  (defn translate [dataset edn-config validity reader writer]
+    (let [config (parse-config edn-config)
+          translators (if-let [t (:config/translators config)] t config)
+          sequence-element (:config/sequence-element config)
+          sequence-selector (if sequence-element
+                              (partial drop-while (comp not (start-element-pred sequence-element)))
+                              identity)
+          feature-identifier (:config/feature-identifier config)
+          feature-identifier (if feature-identifier
+                             (apply comp feature-identifier)
+                             identity)
+          feature-selector (:config/feature-selector config)
+          feature-selector (if feature-selector
+                               (apply comp feature-selector)
+                               identity)
+          ]
+      (binding [*translators* translators
+                *sequence-selector* sequence-selector
+                *feature-identifier* feature-identifier
+                *feature-selector* feature-selector]
+        (process reader writer dataset validity))))
 
-(defn exit [status msg]
-  (println msg)
-  (System/exit status))
+  (defn translate-filesystem [dataset edn-config-location validity in-file out-file]
+    (with-open [reader (clojure.java.io/reader in-file),
+                writer (clojure.java.io/writer out-file)]
+      (translate dataset (slurp edn-config-location) validity reader writer))
+    (shutdown-agents))
 
-(defn implementation-version []
-  (if-let [version (System/getProperty "gml-to-featured.version")]
-    version
-    (-> ^java.lang.Class (eval 'gml_to_featured.runner) .getPackage .getImplementationVersion)))
+  (defn error-msg [errors]
+    (str "The following errors occurred while parsing your command:\n\n"
+         (string/join \newline errors)))
 
-(defn usage [options-summary]
-  (->> ["This program converts xml to featured-ready json. The conversion is done using the provided mappingconfig(uration) specified in edn."
-        ""
-        "Usage: xml2featured [options] datasetname mappingconfig validity inputxml outputfile "
-        ""
-        "Options:"
-        options-summary
-        ""
-        "Please refer to the manual page for more information."]
-       (string/join \newline)))
+  (defn exit [status msg]
+    (println msg)
+    (System/exit status))
 
-(def cli-options
-  [["-h" "--help"]
-   ["-v" "--version"]])
+  (defn implementation-version []
+    (if-let [version (System/getProperty "gml-to-featured.version")]
+      version
+      (-> ^java.lang.Class (eval 'gml_to_featured.runner) .getPackage .getImplementationVersion)))
 
-(defn -main [& args]
-  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
-    ;; Handle help and error conditions
-    (cond
-      (:help options) (exit 0 (usage summary))
-      (:version options) (exit 0 (implementation-version))
-      (not= (count arguments) 5) (exit 1 (usage summary))
-      errors (exit 1 (error-msg errors)))
-    ;; Execute program with options
-    (if (= 5 (count args))
-     (translate-filesystem (nth args 0) (nth args 1) (nth args 2) (nth args 3) (nth args 4))
-     (exit 1 (usage summary)))))
+  (defn usage [options-summary]
+    (->> ["This program converts xml to featured-ready json. The conversion is done using the provided mappingconfig(uration) specified in edn."
+          ""
+          "Usage: xml2featured [options] datasetname mappingconfig validity inputxml outputfile "
+          ""
+          "Options:"
+          options-summary
+          ""
+          "Please refer to the manual page for more information."]
+         (string/join \newline)))
+
+  (def cli-options
+    [["-h" "--help"]
+     ["-v" "--version"]])
+
+  (defn -main [& args]
+    (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+      ;; Handle help and error conditions
+      (cond
+        (:help options) (exit 0 (usage summary))
+        (:version options) (exit 0 (implementation-version))
+        (not= (count arguments) 5) (exit 1 (usage summary))
+        errors (exit 1 (error-msg errors)))
+      ;; Execute program with options
+      (if (= 5 (count args))
+        (translate-filesystem (nth args 0) (nth args 1) (nth args 2) (nth args 3) (nth args 4))
+        (exit 1 (usage summary)))))
