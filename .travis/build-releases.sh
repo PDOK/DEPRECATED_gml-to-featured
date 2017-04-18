90615a2e5616490cd4c1963b68f9d9254322650a
Bas Duineveld <Bas.Duineveld@kadaster.nl>
2017-04-18 12:38:26 +0200

    Fix release builds

--- .travis/build-releases.sh
+++ .travis/build-releases.sh
@@ -3,13 +3,13 @@
 set -ev
 export VERSION=$(printf $(cat VERSION))
 
-if [ ! -f artifacts/featured-to-geoserver-$VERSION-standalone.jar ]; then
+if [ ! -f artifacts/gml-to-featured-$VERSION-standalone.jar ]; then
   $lein with-profile +cli build
-  cp target/featured-to-geoserver-$VERSION-standalone.jar artifacts/
+  cp target/gml-to-featured-$VERSION-standalone.jar artifacts/
 fi
 
 
-if [ ! -f artifacts/featured-to-geoserver-$VERSION-web.jar ]; then
+if [ ! -f artifacts/gml-to-featured-$VERSION-web.jar ]; then
   $lein with-profile +web-jar build
-  cp target/featured-to-geoserver-$VERSION-web.jar artifacts/
+  cp target/gml-to-featured-$VERSION-web.jar artifacts/
 fi
